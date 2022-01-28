b977400d90943560e72851078ebcc3930b692a67
Samuel Juhel <pro@sjuhel.org>
2022-01-28 12:52:52 +0100

    Doc

--- make.bat
+++ make.bat
@@ -1,35 +1,36 @@
 @ECHO OFF
-
 pushd %~dp0
-
 REM Command file for Sphinx documentation
-
 if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
+set SPHINXBUILD=sphinx-build
 )
 set SOURCEDIR=source
 set BUILDDIR=build
-
+set DOCS=..\..\..\gh-pages
 if "%1" == "" goto help
-
+if "%1" == "github" goto github
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
-	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
-	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.https://www.sphinx-doc.org/
-	exit /b 1
+echo.
+echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+echo.installed, then set the SPHINXBUILD environment variable to point
+echo.to the full path of the 'sphinx-build' executable. Alternatively you
+echo.may add the Sphinx directory to PATH.
+echo.
+echo.If you don't have Sphinx installed, grab it from
+echo.http://sphinx-doc.org/
+exit /b 1
 )
-
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
-
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
-
+goto end
+:github
+rmdir /S /Q "%BUILDDIR%"
+%SPHINXBUILD% -M html %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+copy /Y "%BUILDDIR%\html" "%DOCS%"
+echo. 2>"%DOCS%\.nojekyll"
+goto end
 :end
 popd
