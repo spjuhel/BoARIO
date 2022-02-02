860f39796d207c718a9cd70a1af5e88076d882a9
Samuel Juhel <pro@sjuhel.org>
2022-02-02 14:45:11 +0100

    better scripts

--- scripts/aggreg_exio3.py
+++ scripts/aggreg_exio3.py
@@ -18,15 +18,15 @@ parser.add_argument('save_path', type=str, help='The str path to save the pickle
 args = parser.parse_args()
 
 def aggreg(exio_path, aggreg_path, sectors_name_path, save_path=None):
-    rootLogger.info("Parsing exiobase3 from {}".format(exio_path))
+    rootLogger.info("Parsing exiobase3 from {}".format(pathlib.Path(exio_path).absolute()))
     exio3 = pym.parse_exiobase3(path=exio_path)
     rootLogger.info("Done")
-    rootLogger.info("Reading aggregation matrix from sheet 'input' in file {}".format(aggreg_path))
+    rootLogger.info("Reading aggregation matrix from sheet 'input' in file {}".format(pathlib.Path(aggreg_path).absolute()))
     sec_agg_matrix = pd.read_excel(aggreg_path, sheet_name="input", engine="odf", header=None).to_numpy()
     rootLogger.info("Aggregating from {} to {} sectors".format(len(exio3.get_sectors()), sec_agg_matrix.shape[0]))
     exio3.aggregate(sector_agg=sec_agg_matrix)
     rootLogger.info("Done")
-    rootLogger.info("Renaming sectors from {}".format(sectors_name_path))
+    rootLogger.info("Renaming sectors from {}".format(pathlib.Path(sectors_name_path).absolute()))
     with pathlib.Path(sectors_name_path).open('r') as f:
         a = json.load(f)
     exio3.rename_sectors(a)
@@ -35,13 +35,13 @@ def aggreg(exio_path, aggreg_path, sectors_name_path, save_path=None):
     exio3.calc_all()
     rootLogger.info("Done")
     name = save_path+"_"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+".pkl"
-    rootLogger.info("Saving to {}".format(name))
+    rootLogger.info("Saving to {}".format(pathlib.Path(name).absolute()))
     with open(name, 'wb') as f:
         pkl.dump(exio3, f)
         
 if __name__ == '__main__':  
     logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s] %(name)s %(message)s", datefmt="%H:%M:%S")
-    rootLogger = logging.getLogger()
+    rootLogger = logging.getLogger(__name__)
     consoleHandler = logging.StreamHandler(sys.stdout)
     consoleHandler.setFormatter(logFormatter)
     rootLogger.addHandler(consoleHandler)
---
+++ scripts/quick_model_test.py
@@ -0,0 +1,110 @@
+import os
+import sys
+module_path = os.path.abspath(os.path.join('./'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from ario3.simulation import Simulation
+from ario3.indicators import Indicators
+import json
+import pandas as pd
+import numpy as np
+import pathlib
+import csv
+import argparse
+import logging
+
+parser = argparse.ArgumentParser(description="Make a 'quick' run of the model to check if it runs correctly")
+parser.add_argument('exio_path', type=str, help='The str path to the exio3 pickle file')
+parser.add_argument('params_file', type=str, help='The str path to the params to use')
+parser.add_argument('event_file', type=str, help='The str path to the event to simulate')
+parser.add_argument('save_path', type=str, help='The str path to save the results')
+
+args = parser.parse_args()
+
+VA_idx = np.array(['Taxes less subsidies on products purchased: Total',
+       'Other net taxes on production',
+       "Compensation of employees; wages, salaries, & employers' social contributions: Low-skilled",
+       "Compensation of employees; wages, salaries, & employers' social contributions: Medium-skilled",
+       "Compensation of employees; wages, salaries, & employers' social contributions: High-skilled",
+       'Operating surplus: Consumption of fixed capital',
+       'Operating surplus: Rents on land',
+       'Operating surplus: Royalties on resources',
+       'Operating surplus: Remaining net operating surplus'], dtype=object)
+
+def query_yes_no(question, default="no"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+        
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")                             
+
+def quick_test(params, event_template, mrio_path):
+    event = event_template.copy()
+    sim_params = params.copy()
+    model = Simulation(sim_params, mrio_path)
+    model.read_events_from_list([event])
+    model.loop()
+    indic = Indicators.from_storage_path(pathlib.Path(sim_params['storage_dir']), params=sim_params)
+    indic.update_indicators()
+    print(indic.indicators)
+    
+if __name__ == '__main__':  
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s] %(name)s %(message)s", datefmt="%H:%M:%S")
+    rootLogger = logging.getLogger(__name__)
+    consoleHandler = logging.StreamHandler(sys.stdout)
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
+    rootLogger.setLevel(logging.DEBUG)
+    args = parser.parse_args()
+    with pathlib.Path(args.params_file).open('r') as f:
+        params = json.load(f)
+    with pathlib.Path(args.event_file).open('r') as f:
+        event_template = json.load(f)
+    mrio_path = pathlib.Path(args.exio_path)
+    params['storage_dir'] = args.save_path
+    q = """You are about to run the ARIO3 model with the following inputs :
+        - EXIOBASE 3 pickle file : {0}
+        - Model parameters :
+            Storage directory : {1}
+            Steps to simulate : {2}
+            MRIO params file  : {3}
+        - Event file : {4}
+       
+Do you wish to proceed ?
+    """.format(mrio_path.resolve(),
+               pathlib.Path(params['storage_dir']).resolve(),
+               params['n_timesteps'],
+               params['mrio_params_file'],
+               args.event_file
+              )
+    hans = query_yes_no(q,"no")
+    if hans:
+        quick_test(params, event_template, mrio_path)
+        
+    else:
+        print("Aborting")
+        exit()
