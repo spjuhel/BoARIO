97155637a403db410093ae1e7490028e935bdeb3
Samuel Juhel <pro@sjuhel.org>
2022-02-08 10:34:35 +0100

    debuging

--- ario3/__init__.py
+++ ario3/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-from ario3.logging_conf import INFOFORMATTER
+from ario3.logging_conf import INFOFORMATTER, DEBUGFORMATTER
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -8,7 +8,7 @@ logger.setLevel(logging.DEBUG)
 # defines the stream handler
 _ch = logging.StreamHandler()  # creates the handler
 _ch.setLevel(logging.INFO)  # sets the handler info
-_ch.setFormatter(logging.Formatter(INFOFORMATTER))  # sets the handler formatting
+_ch.setFormatter(logging.Formatter(DEBUGFORMATTER))  # sets the handler formatting
 
 # adds the handler to the global variable: log
 logger.addHandler(_ch)
