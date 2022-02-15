908756691bacac93e0c0cdd360463aec7edc6a0a
Samuel Juhel <pro@sjuhel.org>
2022-02-15 16:29:24 +0100

    add .. to module path

--- scripts/quick_model_test.py
+++ scripts/quick_model_test.py
@@ -1,5 +1,9 @@
 import os
 import sys
+module_path = os.path.abspath(os.path.join('../'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
 module_path = os.path.abspath(os.path.join('./'))
 if module_path not in sys.path:
     sys.path.append(module_path)
