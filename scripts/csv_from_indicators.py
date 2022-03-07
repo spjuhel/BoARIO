6874f03424d69f47c121499f88b6999f7b5107ab
Samuel Juhel <pro@sjuhel.org>
2022-03-07 19:17:57 +0100

    versatility

--- scripts/csv_from_indicators.py
+++ scripts/csv_from_indicators.py
@@ -19,7 +19,7 @@ import re
 
 parser = argparse.ArgumentParser(description="Produce csv from json indicators")
 parser.add_argument('folder', type=str, help='The str path to the main folder')
-parser.add_argument('indicator_file', type=str, help='The name of the indicator file to load (indicators.json or prod_indicators.json)')
+#parser.add_argument('indicator_file', type=str, help='The name of the indicator file to load (indicators.json or prod_indicators.json)')
 parser.add_argument('-o', "--output", type=str, help='Path where to save csv')
 logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s] %(name)s %(message)s", datefmt="%H:%M:%S")
 scriptLogger = logging.getLogger("indicators_batch")
@@ -45,20 +45,23 @@ def produce_general_csv(folder,save_path):
     pd.DataFrame(future_df).to_csv(save_path)
 
 def produce_region_prod_loss_csv(folder,save_path):
-    future_df = []
+    future_df = None
     for ind in folder.glob('**/prod_indicators.json'):
         with ind.open('r') as f:
             dico = json.load(f)
 
-        a = pd.Series(ind)
-        df = pd.DataFrame({ind.parent.name:a}).T.to_dict()
-        future_df.append(df)
+        df = pd.DataFrame(dico)
+        if future_df is None:
+            future_df = df.copy()
+        else:
+            future_df.append(df)
 
-    pd.DataFrame(future_df).to_csv(save_path)
+    future_df.to_csv(save_path)
 
 
 if __name__ == '__main__':
     scriptLogger.info('Starting Script')
     args = parser.parse_args()
     folder = pathlib.Path(args.folder).resolve()
-    produce_csv(folder,save_path=args.output,indicator_file=args.indicator_file)
+    produce_general_csv(folder,save_path=args.output+"general.csv")
+    produce_region_prod_loss_csv(folder,save_path=args.output+"prodloss.csv")
