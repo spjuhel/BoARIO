2f876eabc7db6571b9c7cb5094f979e6c8a43fe8
Samuel Juhel <samuel.juhel@zaclys.net>
2022-09-20 09:56:57 +0200

    pipy test

--- boario/__init__.py
+++ boario/__init__.py
@@ -14,7 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import coloredlogs, logging
+import coloredlogs
+import logging
+
+__version__ = "v0.9.4.dev0"
 
 # Create a logger object.
 logger = logging.getLogger(__name__)
