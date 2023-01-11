f9230465aed55b0b0701adb25e4089d98eaaeada
Samuel Juhel <samuel.juhel@zaclys.net>
2023-01-11 10:59:20 +0100

    Possible fix for weird shape bug

--- boario/model_base.py
+++ boario/model_base.py
@@ -925,7 +925,8 @@ class ARIOBaseModel(object):
             matrix_stock_gap = self.calc_matrix_stock_gap(matrix_stock_goal)
         matrix_stock_gap += (np.tile(self.production, (self.n_sectors, 1)) * self.tech_mat)
         if self.order_type == "alt":
-            prod_ratio = np.divide(self.production,self.X_0, where=self.X_0!=0)
+            prod_ratio = np.ones(shape=self.X_0.shape)
+            np.divide(self.production,self.X_0, out=prod_ratio, where=self.X_0!=0)
             Z_prod = self.Z_0 * prod_ratio[:, np.newaxis]
             Z_Cprod = np.tile(self._matrix_I_sum @ Z_prod,(self.n_regions,1))
             out=np.zeros(shape=Z_prod.shape)
