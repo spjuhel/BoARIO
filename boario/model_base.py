15cd2d8f32997fd4bb21bc95c29631a65f37d4d5
spjuhel <pro@sjuhel.org>
2025-07-28 09:22:42 +0200

    fix(final demand): fixes wrong aggregation

--- boario/model_base.py
+++ boario/model_base.py
@@ -1085,9 +1085,9 @@ class ARIOBaseModel:
         )
         final_demand_not_dist = _fast_sum(final_demand_not_met, axis=1)
         final_demand_not_met = final_demand_not_met.reshape(
-            self.n_regions, self.n_sectors, self.n_regions, self.n_fd_cat
+            self.n_regions, self.n_regions, self.n_sectors, self.n_fd_cat
         ).copy()
-        final_demand_not_met = final_demand_not_met.sum(axis=(0, 3)).flatten()
+        final_demand_not_met = final_demand_not_met.sum(axis=(0, 3)).T.flatten()
 
         # final_demand_not_met = _fast_sum(final_demand_not_met, axis=0)
         # avoid -0.0 (just in case)
